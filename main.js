df7e5ba731f685984dd476bb1fd560fa332ec50a
GianmarcoPerini <g.marco.perini@gmail.com>
2021-08-03 15:00:52 +0200

    plus-js

---
+++ main.js
@@ -0,0 +1,2 @@
+document.getElementById("titoloUno") .innerHTML = "Returnal è un gioco degno non come It Takes Two";
+document.getElementById("titoloDue") .innerHTML = "It Takes Two è un gioco degno non come Returnal";
